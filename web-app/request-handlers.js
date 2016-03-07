79337b974a34872593a9345499e64ce95ae65288
Frazier Mork <fraziermork@gmail.com>
2016-03-07 09:10:10 -0800

    request handlers passing response object working

--- web-app/request-handlers.js
+++ web-app/request-handlers.js
@@ -1,10 +1,24 @@
-function start(){
+var exec = require('child_process').exec;
+
+function start(response){
   console.log('request-handler "start" was called.');
-  return 'hello start';
+  // var content = 'empty';
+  
+  exec('ls -lah', function(error, stdout, stderror){
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write(stdout);
+    response.end();
+    // content = stdout;
+  });
+  
+  // return content;
 }
-function upload(){
+function upload(response){
   console.log('request-handler "upload" was called.');
-  return 'hello upload';
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write('Hello upload');
+  response.end();
+  // return 'hello upload';
 }
 module.exports.start = start;
 module.exports.upload = upload;
--- web-app/server.js
+++ web-app/server.js
@@ -6,11 +6,13 @@ function start(route, handle){
     var pathname = url.parse(request.url).pathname;
     console.log('Request for ' + pathname + 'received.');
     
-    var content = route(handle, pathname);
+    route(handle, pathname, response);
     
-    response.writeHead(200, {'Content-Type':'text/plain'});
-    response.write(content);
-    response.end();
+    // var content = route(handle, pathname);
+    
+    // response.writeHead(200, {'Content-Type':'text/plain'});
+    // response.write(content);
+    // response.end();
   }
   
   http.createServer(onRequest).listen(8888);
